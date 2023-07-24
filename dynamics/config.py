49ab8d7ecd7b106dedfc0a6112b9b5d99169aa93
Anupam Mitra <anupam-mitra@users.noreply.github.com>
2023-07-24 14:42:38 -0600

    Add config.py

---
+++ dynamics/config.py
@@ -0,0 +1,7 @@
+import os
+
+data_directory = os.path.join("..", "..", "pkl")
+mps_directory = os.path.join(data_directory, "mps")
+reducedstate_directory = os.path.join(data_directory, "marginals")
+index_directory = os.path.join(data_directory, "index")
+
