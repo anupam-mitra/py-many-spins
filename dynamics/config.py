b71e55c09d6b9e8ab0d089167155063a0c1ef936
Anupam Mitra <anupam-mitra@users.noreply.github.com>
2023-08-01 07:54:24 -0600

    Update config.

--- dynamics/config.py
+++ dynamics/config.py
@@ -2,7 +2,7 @@ import os
 
 data_directory = os.path.join("..", "..", "pkl")
 mps_directory = os.path.join(data_directory, "mps")
-reducedstate_directory = os.path.join(data_directory, "marginals")
+marginal_directory = os.path.join(data_directory, "marginals")
 index_directory = os.path.join(data_directory, "index")
 
 if not os.path.isdir(data_directory):
