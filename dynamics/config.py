207e5040ee4bfa98fdbecb705a8dce643b334544
Anupam Mitra <anupam-mitra@users.noreply.github.com>
2023-08-01 07:59:29 -0600

    Update config.

--- dynamics/config.py
+++ dynamics/config.py
@@ -8,6 +8,9 @@ index_directory = os.path.join(data_directory, "index")
 if not os.path.isdir(data_directory):
     os.makedirs(data_directory)
 
+if not os.path.isdir(index_directory):
+    os.makedirs(index_directory)
+
 if not os.path.isdir(mps_directory):
     os.makedirs(mps_directory)
 
